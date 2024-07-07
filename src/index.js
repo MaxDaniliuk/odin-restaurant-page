6198bff9f8cbf0d6cb2c30bff4bb420052e13d71
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2024-07-07 14:47:14 +0300

    Remove unnecessary comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import loadHomePage from './loadHomePage';
-// import loadContactsPage from './loadContactsPage';
-// import { clearContent, getPreviousContent } from './clearContent';
 import { TabNavigator } from './tab-navigator';
 import './styles/style.css';
 
